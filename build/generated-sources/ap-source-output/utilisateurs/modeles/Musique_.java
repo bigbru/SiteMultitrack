96156749a214c91cd23778bd29f58e851245b2c6
Damien Mostacchi <damien.mostacchi@gmail.com>
2014-05-15 15:38:29 +0200

    changement fichier txt

---
+++ build/generated-sources/ap-source-output/utilisateurs/modeles/Musique_.java
@@ -0,0 +1,16 @@
+package utilisateurs.modeles;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-05-13T15:12:27")
+@StaticMetamodel(Musique.class)
+public class Musique_ { 
+
+    public static volatile SingularAttribute<Musique, Integer> id;
+    public static volatile SingularAttribute<Musique, String> Auteur;
+    public static volatile SingularAttribute<Musique, Double> prix;
+    public static volatile SingularAttribute<Musique, String> Titre;
+
+}
--- build/generated-sources/ap-source-output/utilisateurs/modeles/Utilisateur_.java
+++ build/generated-sources/ap-source-output/utilisateurs/modeles/Utilisateur_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-05-12T21:58:10")
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-05-13T15:12:27")
 @StaticMetamodel(Utilisateur.class)
 public class Utilisateur_ { 
 
