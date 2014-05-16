72d4d9e825fe32bcb9ea1765a0767c9dbdca3c05
Damien Mostacchi <damien.mostacchi@gmail.com>
2014-05-16 16:28:35 +0200

    gestionUser

--- src/java/utilisateurs/gestionnaires/GestionnaireUtilisateurs.java
+++ src/java/utilisateurs/gestionnaires/GestionnaireUtilisateurs.java
@@ -9,6 +9,7 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import utilisateurs.modeles.Chanson;
 import utilisateurs.modeles.Utilisateur;
 
 @Stateless
@@ -20,7 +21,6 @@ public class GestionnaireUtilisateurs {
     private EntityManager em;
 
     private int currentLine = 0;
-    
 
     public void generateUsers(int nb) {
 
@@ -61,12 +61,18 @@ public class GestionnaireUtilisateurs {
     }
 
     public void creerUtilisateursDeBase() {
-        creeUtilisateur("admin", "admin", "admin", "admin");
+        Utilisateur u = creeUtilisateur("admin", "admin", "admin", "admin");
+        u.getListeChansons().add(getSong(1));
+        u.getListeChansons().add(getSong(2));
+        u.getListeChansons().add(getSong(3));
     }
 
     public Utilisateur creeUtilisateur(String nom, String prenom, String login) {
         if (!testUser(login)) {
             Utilisateur u = new Utilisateur(login, nom, prenom);
+            u.getListeChansons().add(getSong(1));
+            u.getListeChansons().add(getSong(2));
+            u.getListeChansons().add(getSong(3));
             em.persist(u);
             return u;
         }
@@ -76,12 +82,24 @@ public class GestionnaireUtilisateurs {
     public Utilisateur creeUtilisateur(String nom, String prenom, String login, String password) {
         if (!testUser(login)) {
             Utilisateur u = new Utilisateur(login, nom, prenom, password);
+            u.getListeChansons().add(getSong(1));
+            u.getListeChansons().add(getSong(2));
+            u.getListeChansons().add(getSong(3));
             em.persist(u);
             return u;
         }
         return null;
     }
-    
+
+    public Chanson getSong(int index) {
+        // Exécution d'une requête équivalente à un select *
+        Query q = em.createQuery("select u from Chanson u");
+        q.setMaxResults(10);
+        q.setFirstResult(index * 10);
+        Chanson c = (Chanson) q.getResultList().get(0);
+        return c;
+    }
+
     public Collection<Utilisateur> getAllUsers() {
         // Exécution d'une requête équivalente à un select *
         Query q = em.createQuery("select u from Utilisateur u");
@@ -145,11 +163,12 @@ public class GestionnaireUtilisateurs {
         Query q = em.createQuery("select u from Utilisateur u where u.login = :login and u.password = :password");
         q.setParameter("password", password);
         q.setParameter("login", login);
-        
+
         Utilisateur user = null;
         if (q.getResultList().size() > 0) {
             user = (Utilisateur) q.getResultList().get(0);
         }
         return user;
     }
+
 }
